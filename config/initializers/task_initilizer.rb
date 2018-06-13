2e93601efbe18f445bbc2e409a3d87a4b437dc5a
José Cordeiro de Oliveira Junior <cordjr@gmail.com>
2018-06-13 00:17:12 -0300

    added price updater service  task initializer

--- config/initializers/task_initilizer.rb
+++ config/initializers/task_initilizer.rb
@@ -1,7 +1,10 @@
 require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler::singleton
+price_updater = PriceUpdaterService.new
 
-scheduler.every '5s'do 
+scheduler.every '40s'do 
+ puts  "runing price updater service"
+ price_updater.run
    
 end
