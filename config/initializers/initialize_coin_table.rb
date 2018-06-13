1e6a6827497a23e7a691f0ff7e90cf9b36060b66
José Cordeiro de Oliveira Junior <cordjr@gmail.com>
2018-06-13 19:12:33 -0300

    merge

--- config/initializers/initialize_coin_table.rb
+++ config/initializers/initialize_coin_table.rb
@@ -5,20 +5,12 @@ COIN_LIST = [
     {symbol:'XRP' , name: 'Ripple', icon_path: 'icons/ripple.png'  },
     {symbol:'LTC' , name: 'Litecoin', icon_path: 'icons/litecoin-logo.png'  }
 ]
-<<<<<<< HEAD
-def init_coin_table 
-    COIN_LIST.each do |c|
-=======
 if Coin.table_exists?
     coin_list.each do |c|
->>>>>>> a8724fd735203bfdb7152cd33784c9d50d4c5e18
         if not Coin.where(symbol: c[:symbol]).take
             Coin.create(c)
         end
     end
 end
-<<<<<<< HEAD
 
 init_coin_table()
-=======
->>>>>>> a8724fd735203bfdb7152cd33784c9d50d4c5e18
